f8c0dd6507edb7e9b051c4da99d58d38feebde24
yangyan <yangyan9791@cvte.com>
2023-11-15 11:46:46 +0800

    文件修改

--- CommonUI/build.gradle.kts
+++ CommonUI/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 apply(from = "../publish-mavencentral.gradle")
@@ -8,24 +8,10 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.common.ui"
         minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
